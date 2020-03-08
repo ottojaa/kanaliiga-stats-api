3a51ee1aeaa041776e5b5ad127e5cd4808d2f9d3
otto.jaakonmaki <otto.jaakonmaki@cgi.com>
2020-03-08 19:39:14 +0200

    something

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -41,7 +41,7 @@ exports.discordCallback = [
 exports.discordAuth = [
   function(req, res) {
     try {
-      const url = `https://discordapp.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${CLIENT_REDIRECT}&response_type=code&scope=identify`;
+      const url = `https://discordapp.com/api/oauth2/authorize?client_id=${CLIENT_ID}&scope=identify&response_type=code&redirect_uri=${CLIENT_REDIRECT}`;
       return apiResponse.successResponseWithData(
         res,
         "Redirect url received",
